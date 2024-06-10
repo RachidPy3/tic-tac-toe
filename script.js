e34c75b190ea446926fcbd6d72c2f1063e7b1648
unknown <mouatacim.med@gmail.com>
2024-06-10 05:29:24 +0200

    project done

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let grid = []
 const holder = document.querySelector(".holder")
+const h1 = document.querySelector("h1")
 let isX = true
+let scoringGrid = Array(9).fill("")
+
+holder.style.display = "grid";
+holder.style.gridTemplateColumns = "repeat(3, 20px)";
+holder.style.gridTemplateRows = "repeat(3, 20px)";
+holder.style.gap = "5px"
 
 function createDiv() {
     const div = document.createElement("div")
@@ -13,12 +20,56 @@ function createDiv() {
     return div
 }
 
+function resetGame() {
+    scoringGrid.fill("");
+    grid.forEach(div => {
+        div.textContent = "";
+    });
+    h1.textContent = "Winner is"
+    isX = false;
+}
+
+function checkWinner() {
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for (const combination of winningCombinations) {
+        const [a, b, c] = combination;
+        if (scoringGrid[a] && scoringGrid[a] === scoringGrid[b] && scoringGrid[a] === scoringGrid[c]) {
+            return scoringGrid[a];
+        }
+    }
+    return null;
+}
+
 function fillElem(event) {
     if (event.target.textContent === "") {
         event.target.textContent = isX ? "x" : "o"
-        
+        scoringGrid[+event.target.getAttribute("index")] = event.target.textContent
+        const winner = checkWinner();
+        if (winner) {
+            h1.textContent += `${winner}`
+            setTimeout(() => {
+                console.log("waiting");
+                resetGame();
+            }, 1000);
+        } else if (!scoringGrid.includes("")) {
+            h1.textContent = "It's a draw!"
+            setTimeout(() => {
+                console.log("waiting");
+                resetGame();
+            }, 1000);
+        }
         isX = !isX
     }
+    console.log(scoringGrid);
 }
 
 function fillArray() {
@@ -29,7 +80,6 @@ function fillArray() {
         grid.push(newDiv)
     }
 }
-
 fillArray()
 grid.forEach((elem) => {
     holder.appendChild(elem)
