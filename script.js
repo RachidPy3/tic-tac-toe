3df6a099ad698a3cfe9089f318d69c2a8adeec22
unknown <mouatacim.med@gmail.com>
2024-06-10 05:02:18 +0200

    added basic functionality

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-let grid = new Array(9)
+let grid = []
 const holder = document.querySelector(".holder")
+let isX = true
 
-const div = document.createElement("div")
-div.style.border = "1px black solid"
-div.style.height = "20px"
-div.style.width = "20px"
+function createDiv() {
+    const div = document.createElement("div")
+    div.style.border = "1px black solid"
+    div.style.height = "20px"
+    div.style.width = "20px"
+    div.style.display = "inline-block"
+    div.style.lineHeight = "20px"
+    div.style.textAlign = "center"
+    return div
+}
 
-grid.forEach(()=>{
-    holder.appendChild(div)
+function fillElem(event) {
+    if (event.target.textContent === "") {
+        event.target.textContent = isX ? "x" : "o"
+        
+        isX = !isX
+    }
+}
+
+function fillArray() {
+    for (let i = 0; i < 9; i++) {
+        const newDiv = createDiv()
+        newDiv.setAttribute("index", i)
+        newDiv.addEventListener("click", fillElem)
+        grid.push(newDiv)
+    }
+}
+
+fillArray()
+grid.forEach((elem) => {
+    holder.appendChild(elem)
 })
-console.log(holder.childNodes)
